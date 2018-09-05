a4f0ce7f37897ebaedcfa8557b2e79e92ec788d1
Brendon Girão <brendongirao@gmail.com>
2018-09-04 21:21:46 -0300

    default button completo

--- js/move.js
+++ js/move.js
@@ -1,1 +1,7 @@
-console.log('testt');
+$( document ).ready(function(){
+    $('.mv-button').each(function (index, element ) {
+        var width = $(element).innerWidth();
+        var height = $(element).innerHeight();
+        $(element).css({"width": width, "height": height});
+    })
+})
