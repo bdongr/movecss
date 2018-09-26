4c182f92e528c9e73814e2ca7e4ea9e59f79feb3
Brendon Girão <brendongirao@gmail.com>
2018-09-26 16:10:40 -0300

    verificação senha inicio

--- js/move.js
+++ js/move.js
@@ -1,4 +1,30 @@
 $( document ).ready(function(){
+    $.fn.getSVG = function (selector) {
+		var svgDoc = this[0].contentDocument;
+		return $(svgDoc);
+    };
+    
+    window.onload = function() {
+        var svg = $("#revealPass").getSVG();
+        var pass = $("#pass");
+        svg.click(function () {
+            if(pass.attr('type')==='password'){
+                pass.attr('type', 'text')
+            } else{
+                pass.attr('type', 'password')
+            }
+        })
+        var svg2 = $("#revealPass2").getSVG();
+        var pass2 = $("#pass2");
+        svg2.click(function () {
+            if(pass2.attr('type')==='password'){
+                pass2.attr('type', 'text')
+            } else{
+                pass2.attr('type', 'password')
+            }
+        })
+    }
+
     $('.mv-button').each(function (index, element ) {
         var width = $(element).innerWidth();
         var height = $(element).innerHeight();
